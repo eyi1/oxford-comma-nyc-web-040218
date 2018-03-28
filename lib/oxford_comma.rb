2d1042c154a99c48db41418b587accbb5f48f0de
eyi1 <ej7729@gmail.com>
2018-03-28 03:10:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,9 @@ if array.length == 2
   array.join(" and ")
 elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
+    array.join(", ")
   else
       array.join
     end
-return array.join(", ")
+
 end
