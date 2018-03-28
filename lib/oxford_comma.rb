879ea2f343b7dcd15d08878822d2df6253f416eb
eyi1 <ej7729@gmail.com>
2018-03-28 03:09:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-if array[0]
-  array.join
-elsif array.length == 2
+
+if array.length == 2
   array.joing(" and ")
 elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
+  else
+      array.join
+    end
 return array.join(", ")
 end
-end
+
